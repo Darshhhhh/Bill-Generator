3e91c3acdf73886f325e8ccd6f9844fdf93d3924
Darshhhhh <contact.shahdarsh@gmail.com>
2023-06-27 21:57:11 +0530

    Frontend Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
-    <>
-      <h1>Hello Darsh</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/create/user" element={<Register />} />
+        {/* <Route
+          path="/user/dashboard"
+          element={<ProtectedRoute Component={DashboardPage} />}
+        /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
