e0b87ed981a1eee763812abe1fc1da3c03db2469
Darshhhhh <contact.shahdarsh@gmail.com>
2023-06-27 23:52:01 +0530

    Login Register UI added

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,6 +1,41 @@
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const LoginHandler = () => {
+    navigate("/dashboard");
+  };
+  return (
+    <>
+      <div className="MainLoginBg flex justify-center items-center">
+        <div className="LoginBox flex flex-col justify-center items-center gap-5 py-5 shadow-lg">
+          <h1 className="font-semibold mb-3">Sign in</h1>
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-60 md:w-80 lg:w-80"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-60 md:w-80 lg:w-80"
+          />
+          <button
+            className="bg-teal-900 text-white hover:bg-transparent hover:text-teal-900 hover:border hover:border-teal-900"
+            onClick={LoginHandler}
+          >
+            Login
+          </button>
+          <p
+            className="text-blue-600 text-sm underline hover:cursor-pointer"
+            onClick={() => navigate("/create/user")}
+          >
+            New User?Click to Sign Up
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- Frontend/src/components/Register/Register.jsx
+++ Frontend/src/components/Register/Register.jsx
@@ -1,8 +1,39 @@
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const navigate = useNavigate();
+
   return (
-    <div>Register</div>
-  )
+    <div className="MainLoginBg flex justify-center items-center">
+      <div className="LoginBox flex flex-col justify-center items-center gap-5 py-5 shadow-lg">
+        <h1 className="font-semibold mb-3">Sign Up</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          className="w-60 md:w-80 lg:w-80"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-60 md:w-80 lg:w-80"
+        />
+        <input
+          type="password"
+          placeholder="Confitm Password"
+          className="w-60 md:w-80 lg:w-80"
+        />
+        <button className="bg-teal-900 text-white hover:bg-transparent hover:text-teal-900 hover:border hover:border-teal-900">
+          Register now
+        </button>
+        <p
+          className="text-blue-600 text-sm underline hover:cursor-pointer"
+          onClick={() => navigate("/")}
+        >
+          Already Have Account?Sign in now
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
